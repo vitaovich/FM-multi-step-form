39b2df53bae060a4c7bfbaa8a01b8bfab09b2d5a
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-25 01:27:30 +0000

    make changes to addons

--- components/stepThreeForm.tsx
+++ components/stepThreeForm.tsx
@@ -1,28 +1,35 @@
 // import iconArcade from "../public/images/icon-arcade.svg"
+import Addon from "@/models/Addon"
 import { useState } from "react"
 
-const StepThreeForm = () => {
-    const [addons, setAddons] = useState<{ title: string, description: string, price: string }[]>(
+const StepThreeForm: React.FC<{yearly: boolean}> = (props) => {
+    const [addons, setAddons] = useState<Addon[]>(
         [
-            { title: 'Online service', description: 'Access to multiplayer games', price: '+$1/mo' },
-            { title: 'Larger storage', description: 'Extra 1TB of cloud save', price: '+$2/mo' },
-            { title: 'Customizable Profile', description: 'Custom theme on your profile', price: '+$3/mo' },
+            new Addon('Online service', 'Access to multiplayer games', 1),
+            new Addon('Larger storage', 'Extra 1TB of cloud save', 2),
+            new Addon('Customizable Profile', 'Custom theme on your profile', 3),
         ]
+    )
+
+    const formattedAddons = addons.map((addon) => (
+        { addon: addon, price: `+$${props.yearly ? addon.price * 10 : addon.price}/${props.yearly ? 'yr' : 'mo'}` }
+    )
     )
 
     return (
         <div className="flex flex-col space-y-4">
+            {'props.yearly: ' + props.yearly}
             {
-                addons.map((addon) => (
-                    <div key={addon.title} className="flex flex-row items-center border rounded-md space-x-4 p-4">
+                formattedAddons.map((addon) => (
+                    <div key={addon.addon.name} className="flex flex-row items-center border rounded-md space-x-4 p-4">
                         <input
                             type="checkbox"
                             name="check"
                             className=""
                         />
                         <div className='flex flex-auto flex-col' >
-                            <h2 className="font-bold">{addon.title}</h2>
-                            <p className="text-CoolGray">{addon.description}</p>
+                            <h2 className="font-bold">{addon.addon.name}</h2>
+                            <p className="text-CoolGray">{addon.addon.description}</p>
                         </div>
                         <p className="text-PurplishBlue">{addon.price}</p>
                     </div>
--- models/Addon.ts
+++ models/Addon.ts
@@ -1,11 +1,13 @@
 class Addon {
     id: string
     name: string
+    description: string
     price: number
 
-    constructor(name: string, price: number) {
+    constructor(name: string, description: string, price: number) {
         this.id = name
         this.name = name
+        this.description = description
         this.price = price
     }
 }
